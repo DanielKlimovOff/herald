598793877c465d899547163c2963bbbf87e690a4
Alexandr Larionov <alarionov93@yandex.ru>
2020-12-03 18:29:09 +0500

    added field age to user

--- core/models.py
+++ core/models.py
@@ -5,31 +5,31 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
+	use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Создает и сохраняет пользователя с введенным им email и паролем.
-        """
-        if not email:
-            raise ValueError('email должен быть указан')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+	def _create_user(self, email, password, **extra_fields):
+		"""
+		Создает и сохраняет пользователя с введенным им email и паролем.
+		"""
+		if not email:
+			raise ValueError('email должен быть указан')
+		email = self.normalize_email(email)
+		user = self.model(email=email, **extra_fields)
+		user.set_password(password)
+		user.save(using=self._db)
+		return user
 
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+	def create_user(self, email, password=None, **extra_fields):
+		extra_fields.setdefault('is_superuser', False)
+		return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
+	def create_superuser(self, email, password, **extra_fields):
+		extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+		if extra_fields.get('is_superuser') is not True:
+			raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(email, password, **extra_fields)
+		return self._create_user(email, password, **extra_fields)
 
 
 # Create your models here.
@@ -37,6 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 	name = models.CharField(max_length=1023, null=False, blank=False, unique=False)
 	phone = models.CharField(max_length=255, null=False, blank=False, unique=False)
 	email = models.CharField(max_length=255, null=False, blank=False, unique=True)
+	age = models.CharField(max_length=255)
 
 	objects = UserManager()
 
